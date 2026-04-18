1dfd7a2dafcde5c2bec7a8902d36b810486c4539
Joshua Mesilane <zindello@Joshuas-MacBook-Air.local>
2026-04-18 18:27:45 +1000

    Switch to manage.sh

--- scripts/03-install-pymc-repeater.sh
+++ scripts/03-install-pymc-repeater.sh
@@ -8,8 +8,8 @@ fi
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 PYMC_SCRIPT_DIR="/tmp/pymc_repeater_install"
-PYMC_REPO_URL="${1:-https://github.com/zindello/pyMC_Repeater.git}"
-PYMC_REPO_BRANCH="${2:-feat/luckfoxInstallSupport}"
+PYMC_REPO_URL="${1:-https://github.com/rightup/pyMC_Repeater.git}"
+PYMC_REPO_BRANCH="${2:-dev}"
 
 git clone --single-branch --branch $PYMC_REPO_BRANCH $PYMC_REPO_URL $PYMC_SCRIPT_DIR
 
